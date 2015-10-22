19fc3a0b21fa336ffc2c8774f4e400340e6ce269
kevinhsu <kevinhsu@synology.com>
2015-10-22 11:00:40 +0800

    python manage.py makemigrations python manage.py migrate

--- excuse/models.py
+++ excuse/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Excuse(models.Model):
+    content = models.TextField()
+
+    def __unicode__(self):
+        return self.content
