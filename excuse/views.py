056f8aa2b30e3983048736a814f371f2f06f358c
kevinhsu <kevinhsu@synology.com>
2015-10-22 10:22:27 +0800

    demo 1 - url and simple HttpResponse

--- excuse/views.py
+++ excuse/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    excuses = [
+        "It was working in my head",
+        "I thought I fixed that",
+        "Actually, that is a feature",
+        "It works on my machine",
+    ]
+
+    return HttpResponse(excuses[0])
